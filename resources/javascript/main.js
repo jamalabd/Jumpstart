5efde0fa47b5e4914e403414a906ee8dee71ce2b
jamalabd <45414121+jamalabd@users.noreply.github.com>
2019-02-20 11:29:49 -0500

    finished

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
   $('.carousel').slick({
-
+     arrows:false,
+     autoplay:true,
+     autoplaySpeed:4000,
+     pauseOnHover:false,
+     pauseOnFocus:false,
+     draggable: false
   });
+  var x = 0;
+  $(window).scroll(function(){
+    var y = $(this).scrollTop();
+    if(y-x > 50){
+      var z = $(".navbar").css("height");
+      $('.navbar').animate({top: '-' + z}, 150);
+        x = y;
+    }else if (x - y > 50) {
+        $('.navbar').animate({top: '0px'}, 150);
+        x = y;
+    }
+  })
 });
